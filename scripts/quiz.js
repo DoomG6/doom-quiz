46f8776896363fa8f95fee64eea6643d6968a579
Jinjing Wu <jinjing.wu@compass.com>
2023-11-30 13:14:09 +0100

    feat: fetch data
    Co-authored-by: linneascode <linneascode@users.noreply.github.com>
    Co-authored-by: Moysis <MoysisPap@users.noreply.github.com>
    Co-authored-by: Edward Wallin <Edw4l@users.noreply.github.com>

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,1 +1,11 @@
 "use strict";
+
+let Url = "https://opentdb.com/api.php?amount=10";
+async function getData() {
+  let res = await fetch(Url);
+  let data = await res.json();
+  return data.results;
+}
+
+let Questions = await getData();
+console.log(Questions);
