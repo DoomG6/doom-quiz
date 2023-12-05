6606e2106a33f3a20e07e8c103ff1a67785b6e55
Jinjing Wu <jinjing.wu@compass.com>
2023-12-05 11:45:31 +0100

    feat: decode html entities

--- scripts/api.js
+++ scripts/api.js
@@ -1,9 +1,30 @@
 import { presetDataSet } from "./dataset.js";
+
 const IN_DEVELOPMENT = true;
 
 export async function getQuiz() {
   let url = "https://opentdb.com/api.php?amount=10";
   let response = IN_DEVELOPMENT ? await presetDataSet() : await fetch(url);
   let data = await response.json();
-  return data.results || [];
+  const result = decode(data.results || []);
+  return result;
+}
+
+function decode(dataset) {
+  let result = dataset.map(function (data) {
+    data.question = decodeHtmlEntities(data.question);
+    data.correct_answer = decodeHtmlEntities(data.correct_answer);
+    let incorrectAnswers = data.incorrect_answers.map(function (answer) {
+      return decodeHtmlEntities(answer);
+    });
+    data.incorrect_answers = incorrectAnswers;
+    return data;
+  });
+  return result;
+}
+
+function decodeHtmlEntities(html) {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(html, "text/html");
+  return doc.documentElement.textContent;
 }
